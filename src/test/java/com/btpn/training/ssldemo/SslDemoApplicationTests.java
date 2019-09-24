c6cd1e7a9b26c409c2c447d0ef1f648d4117a044
Rolan Firwandy <rolan.firwandy@btpn.com>
2019-09-24 22:46:51 +0700

    ssl not yet implemented

--- src/test/java/com/btpn/training/ssldemo/SslDemoApplicationTests.java
+++ src/test/java/com/btpn/training/ssldemo/SslDemoApplicationTests.java
@@ -2,15 +2,30 @@ package com.btpn.training.ssldemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SslDemoApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void getSecuredTest() {
+        ResponseEntity responseEntity = restTemplate.getForEntity("/secured", String.class, "");
+        System.out.println("response: " + responseEntity.getBody().toString());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
 }
