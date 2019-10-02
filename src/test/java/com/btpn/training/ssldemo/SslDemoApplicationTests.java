d7ba8fdeb673626b7289131d038e94a8f3ac9b98
Rolan Firwandy <rolan.firwandy@btpn.com>
2019-10-02 16:11:14 +0700

    use defined port for it test

--- src/test/java/com/btpn/training/ssldemo/SslDemoApplicationTests.java
+++ src/test/java/com/btpn/training/ssldemo/SslDemoApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SslDemoApplicationTests {
 
     @Autowired
